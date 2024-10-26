8a0f48e06e6bfa5414dada4dd9158f4abc6d6b13
raghu9189 <raghu.ballu@gmail.com>
2024-10-26 14:58:31 +0530

    Added: Bean scan

--- src/main/java/com/example/projectone/ProjectoneApplication.java
+++ src/main/java/com/example/projectone/ProjectoneApplication.java
@@ -1,13 +1,18 @@
 package com.example.projectone;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProjectoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectoneApplication.class, args);
+		ApplicationContext apc =  SpringApplication.run(ProjectoneApplication.class, args);
+		for(String a: apc.getBeanDefinitionNames()){
+			System.out.println(a);
+		}
 	}
 
 }
